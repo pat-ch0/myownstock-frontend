7fd9672544f57a36574682c541d4604553494dd5
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2023-12-27 10:04:38 +0100

    US3 : Component and sub component with render

---
+++ src/components/product-tile.js
@@ -0,0 +1,53 @@
+import { Product } from "../js/product-list/product"
+
+export class ProductTile {
+    /**
+     * Product to render
+     * @var Product
+     */
+    #product = null
+
+    #template = `<div class="product-tile">
+        <div class="product-thumbnail">
+            <figure>
+                <img src="{{ productThumbnail }}">
+            </figure>
+        </div>
+        <div class="product-detail">
+            <div class="product-title">
+                {{ productTitle }}
+            </div>
+            <div class="product-stock">
+                <span class="stock-label">Stock : </span>
+                <span class="product-stock">{{ productStock }}
+            </div>
+        </div>
+    `
+    setParameter(paramName, value) {
+        this[paramName] = value
+        return this
+    }
+
+    render() {
+        if (this.#product) {
+            const thumbnail = this.#getThumbnail()
+            this.#template = this.#template.replace(`{{ productThumbnail }}`, thumbnail)
+
+            this.#template = this.#template.replace(`{{ productTitle }}`, this.#product.label)
+
+            this.#template = this.#template.replace(`{{ productStock }}`, this.#product.stock)
+
+            return this.#template
+        }
+
+        throw new Error(`Cannot render without a Product instance. Try to use setParameter('product', value) before rendering`)
+    }
+
+    #getThumbnail() {
+        if (this.#product.hasOwnProperty('thumbnail')) {
+            return `/assets/images/product/${this.#product.thumbnail}`
+        }
+
+        return 'assets/images/product/no-image.png'
+    }
+}
--- src/components/stock.component.js
+++ src/components/stock.component.js
@@ -1,11 +1,60 @@
+import { ProductService } from './../js/product-list/product-service'
+import { ProductDeserializer } from '../js/product-list/product-deserializer'
+import { ProductTile } from './product-tile'
+/**
+ * StockComponent
+ * @version 1.0.0
+ *  - Load product stock
+ *  - Sort stock by alpha order
+ *  - Display stock as simple tile list
+ * @author Jean-Luc Aubert <jean-luc.aubert@aelion.fr>
+ */
 export class StockComponent {
+    /**
+     * Component title
+     * @var string
+     */
     #title = 'My Stock'
-    #template = `<p>Stock works!</p>`
+
+    /**
+     * Template to use to render
+     * @var string
+     */
+    #template = ''
+
+    /**
+     * Product service
+     * @var ProductService
+     */
+    #service = null
+
+    /**
+     * Products to display alphabetical sorted
+     * @var Array<Product>
+     */
+    #products = []
+
+    constructor() {
+        this.#service = new ProductService()
+        this.#onInit()
+    }
+
 
     load() {
         const title = document.querySelector('h1')
         title.innerHTML = this.#title
         const app = document.querySelector('[app]')
         app.innerHTML = this.#template
+    }
+
+    #onInit() {
+        this.#products = ProductDeserializer.deserialize(this.#service.findAll())
+            .sort((p1, p2) => p1.localeCompare(p2))
+        for (const product of this.#products) {
+            const tile = new ProductTile()
+            tile.setParameter('product', product)
+            this.#template += tile.render()
+        }
+
     }
 }
